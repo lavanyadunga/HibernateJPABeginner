8e36d89cad59f525caf1f4110edcb8afc8f8c795
Lavanya Dunga <lavanya.dunga@gmail.com>
2023-01-22 17:00:35 -0500

    IntelliJ config

--- src/main/java/com/ps/test/project/HibernateJPABeginner/App.java
+++ src/main/java/com/ps/test/project/HibernateJPABeginner/App.java
@@ -16,23 +16,15 @@ import com.test.hib.controller.FindUser_Hql;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	// FindUser_Hql u = new FindUser_Hql();
-    	 //u.findUser();
-    	// u.findUserSelect();
-    //u.getRecordbyId();
-    	 //u.getrecords();
-    	 //u.getmaxSalary();
-    	 //u.getmaxSalaryGroupBy();
-    	// u.getNamedQueryExample();
-    	 SessionFactory factory = new Configuration().configure().buildSessionFactory();
-    	   Session session = factory.openSession();
-    	   TypedQuery<Object[]> queryy = session.createQuery( "SELECT U.salary, U.fullname FROM User AS U", Object[].class);
-    	List<Object[]> resultss = queryy.getResultList();
-    	for (Object[] a : resultss) {
-    	   System.out.println("Salary: " + a[0] + ", FullName: " + a[1]); }
+    public static void main( String[] args ) {
+		FindUser_Hql u = new FindUser_Hql();
+		u.findUser();
+		// u.findUserSelect();
+		//u.getRecordbyId();
+		//u.getrecords();
+		//u.getmaxSalary();
+		//u.getmaxSalaryGroupBy();
+		// u.getNamedQueryExample();
 
-    }
-    
+	}
 }
